9532e5ec2b3bb77afba6e588adb8f0cb495593da
kotavr69 <144414802+kotavr69@users.noreply.github.com>
2023-12-07 22:49:36 +0700

    Добавьте файлы проекта.

---
+++ lab8.2/lab8.2.cpp
@@ -0,0 +1,48 @@
+﻿/*Вариант 11
+Дана строка, набранная из слов, набранных заглавными буквами и
+разделенных пробелами (одним или нескольким).
+Вывести строку, содержащую эти же слова,
+разделенные одним пробелом и расположенные в алфавитном порядке.
+*/
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    string str;
+    cout << "Введите слова заглавными буквами" << endl;
+    getline(cin, str, '\n');
+    int n = str.length();
+    for (int i = 0; i < n; i++) {
+        if (((str[i] < 'A') || (str[i] > 'Z')) && (str[i] != ' ')) {
+            cerr << "Ошибка! Не соответствует условию!" << endl;
+            return 0;
+        }
+    }
+
+    char* a = new char[n];
+    int k = 0;
+    for (int i = 0; i < n; i++) {
+        if (str[i] != ' ') {
+            a[k] = str[i];
+            k++;
+        }
+    }
+
+    for (int i = 0; i < k - 1; i++) {
+        for (int j = i; j < k - 1; j++) {
+            if (a[i] > a[j + 1]) {
+                swap(a[i], a[j + 1]);
+            }
+        }
+    }
+
+    for (int i = 0; i < k; i++) {
+        cout << a[i] << ' ';
+    }
+
+    delete[] a;
+}
